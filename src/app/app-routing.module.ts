3b91ec2343f5d163d53fcb54c3c646fe45a597fc
Edgar-Paina <ed01041994@gmail.com>
2020-09-04 18:00:55 -0500

    navbar test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FcomponentComponent } from './fcomponent/fcomponent.component';
+import { ScomponentComponent } from './scomponent/scomponent.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "fcomponent",
+    component: FcomponentComponent
+  },
+  {
+    path:"scomponent",
+    component: ScomponentComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FcomponentComponent } from './fcomponent/fcomponent.component';
 import { ScomponentComponent } from './scomponent/scomponent.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FcomponentComponent,
-    ScomponentComponent
+    ScomponentComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
